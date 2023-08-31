fea62cad6bbe55e01322bf47d74988d4bcb51d94
MarleyDouglas007 <marley.santos2308@gmail.com>
2023-08-31 16:35:32 -0300

    add class logo e css

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,28 @@
 import './globals.css'
-
-
+import styles from './style.module.css'
+/*import {Quicksand} from 'next/font/google'*/
 export const metadata = {
   title: 'Travel landing page',
 }
-
+function Logo() {
+  return (
+    <p className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
+  )
+}
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+
+        <header>
+          <Logo />
+        </header>
+
+        <main>
+          {children}
+        </main>
+
+      </body>
     </html>
   )
 }
